c7acf1b870af1f94eeaf1b0fffb71ba7bc048faf
Peter <40338605+LaptopKing@users.noreply.github.com>
2020-11-06 10:59:33 +0100

    Update Program.cs

--- Sorting Algorithms Practice/Program.cs
+++ Sorting Algorithms Practice/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +11,46 @@ namespace Practice
         static int[] tomb = new int[10];
         static void Main(string[] args)
         {
-            sort_1();
+            // sort_1();
+            int[,] szamok = new int[7, 2] {
+                 {2, 3} ,   
+                 {4, 7} ,  
+                 {0, 0} ,
+                 {2, 9} ,
+                 {1, 2} ,
+                 {8, 3} ,
+                 {0, 5} ,             
+                        };
+            Console.WriteLine("\nKiindulási állapot:");
+            kiir(szamok);
+
+            // Sortin 2D arrays
+            // Feladat: Rendezd a kétdimenziós tömb tartalmát az első oszlop szerint csökkenő sorrendben!
+            // kódod kezdete
+            int num1 = 0;
+            int num2 = 0;
+
+            for (int i = 0; i < szamok.GetLength(0); i++)
+            {
+                for (int l = i + 1; l < szamok.GetLength(0); l++)
+                {
+                    if (szamok[i, 0] < szamok[l, 0])
+                    {
+                        num1 = szamok[i, 0];
+                        num2 = szamok[i, 1];
+                        szamok[i, 0] = szamok[l, 0];
+                        szamok[l, 0] = num1;
+                        szamok[i, 1] = szamok[l, 1];
+                        szamok[l, 1] = num2;
+                    }
+                }
+            }
+            // kódod vége
+
+            Console.WriteLine("\nRendezve:");
+            kiir(szamok);
+
+            Console.ReadKey();
         }
 
         static void random_nums()
@@ -48,6 +87,14 @@ namespace Practice
                     }
                 }
                 Console.WriteLine(tomb[i]);
+            }
+        }
+
+        static void kiir(int[,] nums)
+        {
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                 Console.WriteLine("{0}\t{1}",nums[i, 0],nums[i, 1]);    
             }
         }
     }
