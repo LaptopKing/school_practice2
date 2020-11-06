d40565754c86809f73ceeb657fb02f46a7d8275b
Peter <40338605+LaptopKing@users.noreply.github.com>
2020-11-06 09:58:25 +0100

    Add files via upload

---
+++ Sorting Algorithms Practice/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static int[] tomb = new int[10];
+        static void Main(string[] args)
+        {
+            sort_1();
+        }
+
+        static void random_nums()
+        {
+            Random random = new Random();
+            int num = 0;
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                num = random.Next(0, 100);
+                tomb[i] = num;
+                Console.WriteLine(tomb[i]);
+            }
+        }
+
+        // Egyszerű cserés rendezés (Quicksort)
+        static void sort_1()
+        {
+            random_nums();
+            Console.WriteLine();
+            
+            int num1 = 0;
+            int num2 = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                for (int l = i+1; l < tomb.Length; l++)
+                {
+                    if (tomb[i] > tomb[l])
+                    {
+                        num1 = tomb[i];
+                        num2 = tomb[l];
+                        tomb[i] = tomb[l];
+                        tomb[l] = num1;
+                    }
+                }
+                Console.WriteLine(tomb[i]);
+            }
+        }
+    }
+}
---
+++ Sorting Algorithms Practice/Readme.txt
@@ -0,0 +1,9 @@
+Edit only Program.cs if you are new to this 
+and if you don't know what the other files are doing. Or at least this is
+recommended to do so it won't mess up the files.
+
+You can find a Program.cs file, that is the file that you can modify to build your
+desired ideas and to code it as well.
+
+If you use VS Code and Terminal / Command Prompt to run your application, then use 
+'dotnet run', it will run your whole program!
